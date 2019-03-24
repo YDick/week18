6d2140fadaffc213e2ca030d6be471cbc69414e8
YDick <ydick1998@gmail.com>
2019-03-24 10:11:30 -0400

    parts quantity from int to deci

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_20_023129) do
+ActiveRecord::Schema.define(version: 2019_03_24_140327) do
 
   create_table "locations", force: :cascade do |t|
     t.string "name"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2019_03_20_023129) do
     t.string "name"
     t.string "manufacturer"
     t.decimal "cost"
-    t.integer "quantity"
+    t.decimal "quantity"
   end
 
   create_table "widgets", force: :cascade do |t|
